1ff2edd0f5941a5fd5e7d58bab95b35c1df4acdd
Melissa Lycan <melissamlycan@gmail.com>
2022-06-13 14:14:13 -0600

    updated the responses to adding a new employee

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function startPrompt() {
           addNewRole();
           break;
         case "Add an employee":
-          addNewEmployee();
+          newEmployee();
           break;
         case "Update Employee Role":
           updateRole();
@@ -119,304 +119,132 @@ const addNewRole = () => {
   let questions = [
     {
       type: "input",
-      name: "department_name",
-      message: "What is the new department name?",
+      name: "job_title",
+      message: "What is the new Job Role?",
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "What is the Salary for this Role?",
+    },
+    {
+      type: "input",
+      name: "department_id",
+      message: "What is the department name this role belongs to?",
+      choices: [1, 2, 3, 4, 5],
     },
   ];
-  inquirer.prompt(questions);
-  connection.query("SELECT * FROM roles", (err, res) => {
-    if (err) throw err;
-
-    res.forEach((dep) => {
-      let newDepartment = {
-        name: dep.name,
-        value: dep.id,
-      };
-      departments.push(newDepartment);
+  inquirer
+    .prompt(questions)
+    .then((response) => {
+      const query = `INSERT INTO roles (job_title, salary, department_id) VALUES(?)`;
+      connection.query(
+        query,
+        [[response.job_title, response.salary, response.department_id]],
+        (err, res) => {
+          if (err) throw err;
+          console.log(
+            `Successfully inserted ${response.title} role at id ${res.insertId}`
+          );
+          startPrompt();
+        }
+      );
+    })
+    .catch((err) => {
+      console.error(err);
     });
-
-    //questions for roles
-    let questions = [
-      {
-        type: "input",
-        name: "title",
-        message: "what is the title of the new role?",
-      },
-      {
-        type: "input",
-        name: "salary",
-        message: "what is the salary of the new role?",
-      },
-      {
-        type: "list",
-        name: "department",
-        choices: departments,
-        message: "which department is this role in?",
-      },
-    ];
-
-    inquier
-      .prompt(questions)
-      .then((response) => {
-        const query = `INSERT INTO ROLE (title, salary, department_id) VALUES`;
-        connection.query(
-          query,
-          [[response.title, response.salary, response.department]],
-          (err, res) => {
-            if (err) throw err;
-            console.log(
-              `Successfully inserted ${response.title} role at id ${res.insertId}`
-            );
-            startPrompt();
-          }
-        );
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  });
 };
 
 const newEmployee = () => {
-  //get all employees
-  connection.query("SELECT * FROM EMPLOYEE", (err, emplRes) => {
-    if (err) throw err;
-    const employeeChoice = [
-      {
-        name: "None",
-        value: 0,
-      },
-    ]; //an employee could have no manager
-    emplRes.forEach(({ first_name, last_name, id }) => {
-      employeeChoice.push({
-        name: first_name + " " + last_name,
-        value: id,
-      });
-    });
+  let questions = [
+    {
+      type: "input",
+      message: "What is the new employees first name?",
+      name: "first_name",
+    },
+    {
+      type: "input",
+      message: "What is the new employees last name?",
+      name: "last_name",
+    },
+    {
+      type: "list",
+      message: "What is their role in the department",
+      choices: [1, 2, 3],
+      name: "job_title",
+    },
+    {
+      type: "list",
+      message: "What is their managers id?",
+      choices: [1, 2, 3, 4, 5, "NULL"],
+      name: "manager_id",
+    },
+    {
+      type: "list",
+      message: "What is their job_title",
+      choices: ["Manager", "Engineer", "Intern", "Sales Person"],
+      name: "job_title",
+    },
+    {
+      type: "list",
+      message: "What is the Salary of the new employee?",
+      choices: [60000, 65000, 70000, 75000, 80000, 85000, 90000, 95000, 100000],
+      name: "salary",
+    },
+    {
+      type: "list",
+      message: "What is their department name?",
+      choices: ["Marketing", "Development", "Finance", "Operations", "Sales"],
+      name: "department_name",
+    },
+  ];
 
-    //get all the role list to make choice of employee's role
-    connection.query("SELECT * FROM ROLE", (err, rolRes) => {
-      if (err) throw err;
-      const roleChoice = [];
-      rolRes.forEach(({ title, id }) => {
-        roleChoice.push({
-          name: title,
-          value: id,
-        });
-      });
+  inquirer
+    .prompt(questions)
+    .then((response) => {
+      const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)`;
+      const query2 = `INSERT INTO roles (job_title, salary) VALUES (?)`;
+      const query3 = `INSERT INTO department(department_name) VALUES (?)`;
 
-      let questions = [
-        {
-          type: "input",
-          message: "What is the employee id?",
-          name: "id",
-        },
-        {
-          type: "input",
-          message: "What is the new employees first name?",
-          name: "first_name",
-        },
-        {
-          type: "input",
-          message: "What is the new employees last name?",
-          name: "last_name",
-        },
-
-        {
-          type: "list",
-          message: "What is their department?",
-          choices: ["Marketing", "Finance", "Development", "Operations"],
-          name: "department",
-        },
-        {
-          type: "input",
-          message: "What is their role in the department",
-          choices: ["Manager", "Engineer", "Intern"],
-          name: "job_title",
-        },
-      ];
-
-      inquirer
-        .prompt(questions)
-        .then((response) => {
-          const query = `INSERT INTO EMPLOYEE (first_name, last_name, role_id, manager_id) VALUES (?)`;
-          let manager_id =
-            response.manager_id !== 0 ? response.manager_id : null;
-          connection.query(
-            query,
-            [
-              [
-                response.first_name,
-                response.last_name,
-                response.role_id,
-                manager_id,
-              ],
-            ],
-            (err, res) => {
-              if (err) throw err;
-              console.log(
-                `successfully inserted employee ${response.first_name} ${response.last_name} with id ${res.insertId}`
-              );
-              startPrompt();
-            }
-          );
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    });
-  });
-};
-
-function app() {
-  function newEmployee() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "What is the employee id?",
-          name: "id",
-        },
-        {
-          type: "input",
-          message: "What is the new employees first name?",
-          name: "first_name",
-        },
-        {
-          type: "input",
-          message: "What is the new employees last name?",
-          name: "last_name",
-        },
-
-        {
-          type: "list",
-          message: "What is their department?",
-          choices: ["Marketing", "Finance", "Development", "Operations"],
-          name: "department",
-        },
-        {
-          type: "input",
-          message: "What is their role in the department",
-          choices: ["Manager", "Engineer", "Intern"],
-          name: "job_title",
-        },
-      ])
-      .then((response) => {
-        const employee = new Employee(
-          response.id,
-          response.first_name,
-          response.last_name,
-          response.role_id
-        );
-        teamMembers.push(manager);
-        idArray.push(response.id);
-        createTeam();
-      });
-  }
-
-  function createTeam() {
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          message: "What type of team member would you like to add?",
-          choices: ["engineer", "intern", "No One else to add"],
-          name: "role",
-        },
-      ])
-      .then((response) => {
-        switch (response.role) {
-          case "engineer":
-            addEngineer();
-            break;
-
-          case "intern":
-            addIntern();
-            break;
-
-          default:
-            buildTeam();
+      connection.query(
+        query,
+        [
+          [
+            response.first_name,
+            response.last_name,
+            response.role_id,
+            response.manager_id,
+          ],
+        ],
+        (err, res) => {
+          if (err) throw err;
+          console.table(res);
         }
-      });
-  }
+      ),
+        connection.query(
+          query2,
+          [[response.job_title, response.salary]],
+          (err, res) => {
+            if (err) throw err;
+            console.table(res);
+          }
+        ),
+        connection.query(
+          query3,
+          [[response.department_name]],
+          (err, res) => {
+            if (err) throw err;
+            console.log(res);
+          },
 
-  function addEngineer() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "What is your team engineers name?",
-          name: "name",
-        },
-        {
-          type: "input",
-          message: "What is the engineers id?",
-          name: "id",
-        },
-        {
-          type: "input",
-          message: "What is their email?",
-          name: "email",
-        },
-        {
-          type: "input",
-          message: "What is their github username?",
-          name: "github",
-        },
-      ])
-      .then((response) => {
-        const engineer = new Engineer(
-          response.name,
-          response.id,
-          response.email,
-          response.github
+          startPrompt()
         );
-
-        teamMembers.push(engineer);
-        idArray.push(response.id);
-        createTeam();
-      });
-  }
-
-  function addIntern() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "What is your interns name?",
-          name: "name",
-        },
-        {
-          type: "input",
-          message: "What is the interns id?",
-          name: "id",
-        },
-        {
-          type: "input",
-          message: "What is their email?",
-          name: "email",
-        },
-        {
-          type: "input",
-          message: "What is school did they attend?",
-          name: "school",
-        },
-      ])
-      .then((response) => {
-        const intern = new Intern(
-          response.name,
-          response.id,
-          response.email,
-          response.school
-        );
-        teamMembers.push(intern);
-        idArray.push(response.id);
-        createTeam();
-      });
-  }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 
   const updateRole = () => {
-    //get all the employee list
     connection.query("SELECT * FROM EMPLOYEE", (err, emplRes) => {
       if (err) throw err;
       const employeeChoice = [];
@@ -426,52 +254,52 @@ function app() {
           value: id,
         });
       });
+    });
 
-      //get all the role list to make choice of employee's role
-      connection.query("SELECT * FROM ROLE", (err, rolRes) => {
-        if (err) throw err;
-        const roleChoice = [];
-        rolRes.forEach(({ title, id }) => {
-          roleChoice.push({
-            name: title,
-            value: id,
-          });
+    //get all the role list to make choice of employee's role
+    connection.query("SELECT * FROM ROLE", (err, res) => {
+      if (err) throw err;
+      const roleChoice = [];
+      rolRes.forEach(({ job_title, id }) => {
+        roleChoice.push({
+          name: job_title,
+          value: id,
         });
-
-        let questions = [
-          {
-            type: "list",
-            name: "id",
-            choices: employeeChoice,
-            message: "whose role do you want to update?",
-          },
-          {
-            type: "list",
-            name: "role_id",
-            choices: roleChoice,
-            message: "what is the employee's new role?",
-          },
-        ];
-
-        inquier
-          .prompt(questions)
-          .then((response) => {
-            const query = `UPDATE EMPLOYEE SET ? WHERE ?? = ?;`;
-            connection.query(
-              query,
-              [{ role_id: response.role_id }, "id", response.id],
-              (err, res) => {
-                if (err) throw err;
-
-                console.log("successfully updated!");
-                startPrompt();
-              }
-            );
-          })
-          .catch((err) => {
-            console.error(err);
-          });
       });
+
+      let questions = [
+        {
+          type: "list",
+          name: "id",
+          choices: employeeChoice,
+          message: "Whose role do you want to update?",
+        },
+        {
+          type: "list",
+          name: "role_id",
+          choices: roleChoice,
+          message: "What is the employee's new role?",
+        },
+      ];
+
+      inquirer
+        .prompt(questions)
+        .then((response) => {
+          const query = `UPDATE EMPLOYEE SET ? WHERE ?? = ?;`;
+          connection.query(
+            query,
+            [{ role_id: response.role_id }, "id", response.id],
+            (err, res) => {
+              if (err) throw err;
+
+              console.log("successfully updated!");
+              startPrompt();
+            }
+          );
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     });
   };
-}
+};
